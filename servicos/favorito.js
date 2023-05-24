f87e26b0b35a46f3844c0f94dc992471375fbf56
Daniela Leguari <danielaleguari@outlook.com>
2023-05-24 13:52:07 -0300

    servico de livros favoritos

---
+++ servicos/favorito.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+
+const getLivrosFavoritos = () => {
+    return JSON.parse(fs.readFileSync("./favoritos.json"));
+};
+
+const deletaLivroFavoritoporId = (id) => {
+    const livros = JSON.parse(fs.readFileSync("./favoritos.json"));
+
+    const livrosFiltrados = livros.filter(livro => livro.id !== id);
+
+    fs.writeFileSync("favoritos.json", JSON.stringify(livrosFiltrados));
+
+}
+
+const insereLivroFavorito = (id) => {
+    const livros = JSON.parse(fs.readFileSync("./livros.json"));
+    const favoritos = JSON.parse(fs.readFileSync("./favoritos.json"));
+
+    const livroInserido = livros.find(livro => livro.id === id);
+    const novaListaDeLivrosFavoritos = [...favoritos, livroInserido];
+
+    fs.writeFileSync("./livros.json", JSON.stringify(novaListaDeLivrosFavoritos));
+}
+
+module.exports = {
+    getLivrosFavoritos,
+    deletaLivroFavoritoporId,
+    insereLivroFavorito,
+}
